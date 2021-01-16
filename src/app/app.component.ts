c768ef0973457c99aeb822b0058552d130529aa5
avinashkotyada <2019ucs0089@iitjammu.ac.in>
2021-01-16 18:06:01 +0530

    question1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  inputtext : number
+  inputtext : number = null
   Number : number
 
 
   change(event)
    {this.inputtext = event.target.value
-  
+
   }
 
   generateNum(){
-    if(this.inputtext%2===0){
-     this.Number= this.inputtext*this.inputtext-1
-    }else{
-     this.Number = this.inputtext*this.inputtext+1
+    if(this.inputtext>0){
+
+      if(this.inputtext%2===0){
+        this.Number= this.inputtext*this.inputtext-1
+       }else{
+        this.Number = this.inputtext*this.inputtext+1
+       }
+
+
     }
+
   }
 }
